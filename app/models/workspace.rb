2bce2ed3fd82beccff6749e14bc1342a1706b315
acostayuukichi <acostayuukichi@gmail.com>
2024-05-01 11:02:25 +0800

    adds validations and scope to workspace

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,2 +1,18 @@
 class Workspace < ApplicationRecord
+    validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp }, allow_blank: true
+    validates :description, allow_blank: true
+    
+    enum visibility: [:public, :private]
+    enum tier: [:free, :standard, :premium, :enterprise]
+
+    #visibility scope
+    scope :public, -> { where(visibility: 'public')}
+    scope :private, -> { where(visibility: 'private')}
+
+    #tier scope
+    scope :free, -> { where(tier: 'free')}
+    scope :standard, -> { where(tier: 'standard')}
+    scope :premium, -> { where(tier: 'premium')}
+    scope :enterprise, -> { where(tier: 'enterprise')}
+
 end
