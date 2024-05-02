bfa5d66381d2919f1da7dbc431f65d475a7ed647
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 12:40:38 +0800

    adds position in list table

--- db/migrate/20240502041725_create_lists.rb
+++ db/migrate/20240502041725_create_lists.rb
@@ -3,6 +3,7 @@ class CreateLists < ActiveRecord::Migration[7.1]
     create_table :lists do |t|
       t.references :board, foreign_key: true, null: false
       t.string :name, null: false
+      t.integer :position
 
       t.timestamps
     end
