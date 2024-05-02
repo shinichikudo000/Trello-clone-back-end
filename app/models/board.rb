f49c9feb905f168f0d68fe441a48a186e7ffdfc2
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 14:39:39 +0800

    adds association, validation and scope in board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,16 @@
 class Board < ApplicationRecord
+    belongs_to :workspace
+    
     has_many :lists, dependent: :destroy
+
+    validates :name, presence: true
+    validates :description, allow_blank: true
+
+    enum :visibility [:workspace, :public, :private, :organization]
+
+    scope :workspace_visible, -> { where(visibility: 'workspace')}
+    scope :public, -> { where(visibility: 'public')}
+    scope :private, -> { where(visibility: 'private')}
+    scope :organization_visible, -> { where(visibility: 'oranization')}
+    
 end
