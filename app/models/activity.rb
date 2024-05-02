89dd8382d3a734d7a9c929f54b013605c1b0d24b
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 15:46:56 +0800

    adds association and scope in activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,13 @@
 class Activity < ApplicationRecord
+    belongs_to :user
+    belongs_to :board
+    belongs_to :card
+
+    validates :activity, presence: true
+
+    enum :tag, [:create, :update, :comment]
+
+    scope :create, -> { where(tag: 'create')}
+    scope :update, -> { where(tag: 'update')}
+    scope :comment, -> { where(tag: 'comment')}
 end
