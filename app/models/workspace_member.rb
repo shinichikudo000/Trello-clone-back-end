22bd0fa122743bd0ecb3729cb29edfe9945b124e
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 10:49:31 +0800

    adds scope and validation in workspace member

--- app/models/workspace_member.rb
+++ app/models/workspace_member.rb
@@ -3,5 +3,10 @@ class WorkspaceMember < ApplicationRecord
     belongs_to :workspace
 
     validates :user_id, uniqueness: { scope: :workspace_id }
+
+    enum :role [:member, :admin]
+
+    scope :member, -> { where(role: 'member')}
+    scope :admin, -> { where(role: 'admin')}
     
 end
