b96a83bcda7296dd7bc3ab7c5571f1142cea3948
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 10:23:26 +0800

    delete some code

--- db/migrate/20240501052219_create_boards.rb
+++ db/migrate/20240501052219_create_boards.rb
@@ -4,9 +4,7 @@ class CreateBoards < ActiveRecord::Migration[7.1]
       t.string :name, null: false
       t.integer :visibility, default: 0
       t.string :description, default: nil
-      t.integer :role, default: 0
       t.check_constraint 'visibility_check',  'visibility <= 3'
-      t.check_constraint 'role_check', 'role <= 1'
 
       t.timestamps
     end
