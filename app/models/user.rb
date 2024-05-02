45735184f1a9d3e6d22d711890fbb5c82a5656f0
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 14:45:49 +0800

    adds association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :api
+
+  has_many :board_members
+  has_many :workspace_member
+  has_many :activities
+  has_many :comments
+
 end
