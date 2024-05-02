679d2ba5bec6db5552b7add1fcd1d665b93de731
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 13:35:30 +0800

    adds validation and association in card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,16 @@
 class Card < ApplicationRecord
+    belongs_to :board
+    belongs_to :list
+
+    has_many :card_labels, dependent: :destroy
+    has_many :comments, dependent: :destroy 
+
+    validates :title, presence: true
+    validates :description, allow_blank: true
+    validates :due_date, allow_blank: true
+    validates :start_date, allow_blank: true
+    validates :due_date_reminder, allow_blank: true
+    validates :is_complete, inclusion: { in: [true, false] }
+    validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+
 end
