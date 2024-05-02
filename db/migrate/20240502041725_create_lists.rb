3129b0e710324ac4ce9907bd5cdac317de040767
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 12:23:02 +0800

    adds migration file list

--- db/migrate/20240502041725_create_lists.rb
+++ db/migrate/20240502041725_create_lists.rb
@@ -2,7 +2,8 @@ class CreateLists < ActiveRecord::Migration[7.1]
   def change
     create_table :lists do |t|
       t.references :board, foreign_key: true, null: false
-      
+      t.string :name, null: false
+
       t.timestamps
     end
   end
