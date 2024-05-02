ea334bfa564cf57eda9aaff1be57e0f4dcfb9553
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 14:43:12 +0800

    adds association, scope in board_member

--- app/models/board_member.rb
+++ app/models/board_member.rb
@@ -1,2 +1,9 @@
 class BoardMember < ApplicationRecord
+    belongs_to :board
+    belongs_to :user
+
+    enum :role, [:member, :admin]
+    
+    scope :member, -> { where(role: 'member')}
+    scope :admin, -> { where(role: 'admin')}
 end
