674a635dcfdffd0b081d3b916040230e9b7aa942
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 10:21:50 +0800

    adds constraint to workspace member role

--- db/migrate/20240501041950_create_workspace_members.rb
+++ db/migrate/20240501041950_create_workspace_members.rb
@@ -3,7 +3,9 @@ class CreateWorkspaceMembers < ActiveRecord::Migration[7.1]
     create_table :workspace_members do |t|
       t.references :user, foreign_key: true, null: false
       t.references :workspace, foreign_key: true, null: false
-
+      t.integer :role, default: 0
+      t.check_constraint 'role_check', 'role <= 1'
+      
       t.timestamps
     end
 
