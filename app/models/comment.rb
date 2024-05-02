d03cb51507ce106ea372946f896789911b6612da
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 15:36:29 +0800

    adds association and validatio to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :card
+
+    validates :comment, presence: true
+    validates :is_edited, inclusion: { in: [true, false] }
 end
