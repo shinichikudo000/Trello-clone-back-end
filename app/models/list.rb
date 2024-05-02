eef55f6ec4d88d319f93a80587f4eed2c77116d9
acostayuukichi <acostayuukichi@gmail.com>
2024-05-02 15:26:31 +0800

    adds association, validation to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ApplicationRecord
+    belongs_to :board
+
+    has_many :cards
+
+    validates :name, presence: true
+    validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    
 end
