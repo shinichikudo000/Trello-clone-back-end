c5c075cc503e8e0c17d422fac3c0b823c9c9551b
acostayuukichi <acostayuukichi@gmail.com>
2024-05-03 13:54:29 +0800

    adds helper to create initial workspace for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,18 @@ class User < ApplicationRecord
   has_many :workspace_member
   has_many :activities
   has_many :comments
-  
+
+  after_create :initial_create_user
+
+  private
+
+  def initial_create_user
+    ActiveRecord::Base.transaction do
+      workspace = Workspace.create(name: 'Trello Workspace', visibility: 'private', tier: 'free')
+      workspace_admin = workspace.workspace_members.build(user: self, role: 'admin')
+      board = workspace.boards.build(name: 'My Trello Board', visibility: 'workspace')
+      board_admin = board.board_members.build(user: self, role: 'admin')
+      workspace.save!
+    end
+  end
 end
