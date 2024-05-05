3bd84c7c958faec73a6875d8e40fa2036b24b5d1
acostayuukichi <acostayuukichi@gmail.com>
2024-05-05 15:49:12 +0800

    adds create in workspace controller

--- app/controllers/api/v1/workspace_controller.rb
+++ app/controllers/api/v1/workspace_controller.rb
@@ -1,17 +1,30 @@
-class API::V1::WorkspaceController < ApplicationRecord
+class API::V1::WorkspaceController < ApplicationController
     def index
-
+        @guest_workspaces = guest_workspaces
+        @member_workspaces = member_workspaces
+        render json: {@guest_workspaces, @member_workspaces}
+    end
+    
+    def create
+        create workspace with the workspace params
+        if save!
+            workspace.tier :free
+            workspace.visibility :private
+            workspace.short_name = workspace id
+            
     end
 
     private
-
-    def admin_workspace
-    end
-
-    def guest_workspace
-    end
-
-    def member_workspace
-    end
     
+    def workspace_params
+        params.require(:workspace).permit(:name, :short_name, :description, :website, :visibility, :tier)
+    end
+
+    def guest_workspaces
+        WorkspaceMember.where(user_id: current_user.id, role: :guest).includes(:workspace).map(&:workspace)
+    end
+  
+    def member_workspaces
+        WorkspaceMember.where(user_id: current_user.id, role: [:member, :admin]).includes(:workspace).map(&:workspace)
+    end
 end
