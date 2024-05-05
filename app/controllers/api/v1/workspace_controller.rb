54c8e8a5f6aae8f50d4cf6d4dc975db7dd53359d
acostayuukichi <acostayuukichi@gmail.com>
2024-05-05 15:11:14 +0800

    add some pre methods in workspace

---
+++ app/controllers/api/v1/workspace_controller.rb
@@ -0,0 +1,17 @@
+class API::V1::WorkspaceController < ApplicationRecord
+    def index
+
+    end
+
+    private
+
+    def admin_workspace
+    end
+
+    def guest_workspace
+    end
+
+    def member_workspace
+    end
+    
+end
