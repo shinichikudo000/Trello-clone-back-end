bf6f544ca6fa3b05e48db33b8c692d32bf8a0cd9
acostayuukichi <acostayuukichi@gmail.com>
2024-05-06 17:58:07 +0800

    adds destroy in boards controller

--- app/controllers/api/v1/workspace_controller.rb
+++ app/controllers/api/v1/workspace_controller.rb
@@ -1,10 +1,15 @@
 class API::V1::WorkspaceController < ApplicationController
+    before_action :set_workspace, only: [:show, :destroy]
+
     def index
         @guest_workspaces = guest_workspaces
         @member_workspaces = member_workspaces
         render json: {@guest_workspaces, @member_workspaces}
     end
     
+    def show
+        render json: @workspace.include(:board)
+    end
     def create
         ActiveRecord::Base.transaction do 
             @workspace = Workspace.new(workspace_params)
@@ -19,8 +24,17 @@ class API::V1::WorkspaceController < ApplicationController
         end
     end
 
+    def destroy
+        @workspace.destroy!
+        head :no_content
+    end
+
     private
     
+    def set_workspace
+        @workspace = Workspace.find(params[:workspace_id])
+    end
+
     def workspace_params
         params.require(:workspace).permit(:name, :short_name, :description, :website, :visibility, :tier)
     end
